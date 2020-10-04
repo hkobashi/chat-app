00bc607e5b14c15c2db61979b1050d9fab80f933
hkobashi <71940017+hkobashi@users.noreply.github.com>
2020-10-04 21:01:17 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,11 @@ class Message < ApplicationRecord
   belongs_to :room
   has_one_attached :image
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+  #validatesにunlessaオプションを付けることで
+  #画像かテキストが入っていればテーブルに登録できるようになる。
+
+  def was_attached?
+    self.image.attached?
+  end
 end
