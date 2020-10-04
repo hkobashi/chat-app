702c4857ae4080c211f7e529aada2b111ed71bc0
hkobashi <71940017+hkobashi@users.noreply.github.com>
2020-10-04 14:27:38 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    #生成したインスタンスにprivateメソッドの結果を代入
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    #messageテーブルから送信フォーム内のデータ(conten)を取り出して
+    #roomテーブルに紐付いているcurrent_user.idを連結させる
+  end
+
 end
